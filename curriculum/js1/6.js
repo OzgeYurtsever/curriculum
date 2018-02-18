7d2b480744a0a5b4f3498fb6553ac923743b7f45
Ozge <ozge@garagescript.org>
2018-02-17 21:21:47 -0800

    solved problem 6

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,22 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, sum = 0, run = 1) => {
+  if (a === 0) {
+    return;
+  } 
+
+  let divisor = a - run;
+  if (divisor === 1) {
+    return sum;
+  }
+
+  run++;
+
+  if (a % divisor === 0) {
+    sum += divisor;
+  }
+  return solution(a, sum, run);
 };
 
 module.exports = {
